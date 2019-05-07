069eae39bda0f1a60d1db2d7924e58faf1cdf73d
OSAKANA TARO <osakanataro@gmail.com>
2019-05-07 14:32:19 +0900

    コマンドプロンプトのタイトル表示を変更

--- showroom-live-ringo_kujo.bat
+++ showroom-live-ringo_kujo.bat
@@ -1,5 +1,6 @@
 @echo off
 
+title ShowroomLive:Ringo Kujo
 :startloop
 
 powershell -sta -ExecutionPolicy Unrestricted -File %0\..\showroom-live.ps1 -roomurl "https://www.showroom-live.com/ringo-005" -record 
